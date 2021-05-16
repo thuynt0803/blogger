8068b311c14c2c7c78030372588a2a4c4f314007
Nguyen Thuy <thuy83.tmu@gmail.com>
2021-05-17 00:43:29 +0700

    all done

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # NOTE
 
-
     1. Downloads Fonts
     2. Downloads Font Awesome
     3. Downloads Jquery
     4. Downloads owl-carousel: Owl Carousel là thư viện của Jquery được sử dụng làm slide Responsive
+    5. Downloads AOS: Animate On Scroll library 
+
+
+# A beautiful website =))
--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+};
 $(document).ready(function() {
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
@@ -14,7 +28,17 @@ $(document).ready(function() {
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
 
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html,body').animate({ scrollTop: 0 }, 3000)
+    })
+
+
+    //  AOS
+    AOS.init();
 })
